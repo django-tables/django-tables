b51bfd46749c87a52eed63f2bf23b5d220216507
Jason Ward <jason.ward@policystat.com>
2019-04-24 22:21:54 -0400

    refs #4: Flake8

--- django_tables/app/templatetags/tables.py
+++ django_tables/app/templatetags/tables.py
@@ -16,9 +16,9 @@ import urllib
 import tokenize
 import StringIO
 from django import template
-from django.utils.safestring import mark_safe
 
 register = template.Library()
+
 
 class SetUrlParamNode(template.Node):
     def __init__(self, changes):
@@ -26,7 +26,8 @@ class SetUrlParamNode(template.Node):
 
     def render(self, context):
         request = context.get('request', None)
-        if not request: return ""
+        if not request:
+            return ""
 
         # Note that we want params to **not** be a ``QueryDict`` (thus we
         # don't use it's ``copy()`` method), as it would force all values
@@ -34,8 +35,10 @@ class SetUrlParamNode(template.Node):
         params = dict(request.GET)
         for key, newvalue in self.changes.items():
             newvalue = newvalue.resolve(context)
-            if newvalue=='' or newvalue is None: params.pop(key, False)
-            else: params[key] = unicode(newvalue)
+            if newvalue == '' or newvalue is None:
+                params.pop(key, False)
+            else:
+                params[key] = unicode(newvalue)
         # ``urlencode`` chokes on unicode input, so convert everything to
         # utf8. Note that if some query arguments passed to the site have
         # their non-ascii characters screwed up when passed though this,
@@ -46,27 +49,39 @@ class SetUrlParamNode(template.Node):
         # address bar results in "%E4" being passed to the server (in
         # iso-8859-1), but Django might expect utf-8, where ä would be
         # "%C3%A4"
+
         def mkstr(s):
-            if isinstance(s, list): return map(mkstr, s)
-            else: return (isinstance(s, unicode) and [s.encode('utf-8')] or [s])[0]
+            if isinstance(s, list):
+                return map(mkstr, s)
+            else:
+                return (isinstance(s, unicode) and [s.encode('utf-8')] or [s])[0]  # noqa
         params = dict([(mkstr(k), mkstr(v)) for k, v in params.items()])
         # done, return (string is already safe)
         return '?'+urllib.urlencode(params, doseq=True)
+
 
 def do_seturlparam(parser, token):
     bits = token.contents.split()
     qschanges = {}
     for i in bits[1:]:
         try:
-            a, b = i.split('=', 1); a = a.strip(); b = b.strip()
-            keys = list(tokenize.generate_tokens(StringIO.StringIO(a).readline))
+            a, b = i.split('=', 1)
+            a = a.strip()
+            b = b.strip()
+            keys = list(
+                tokenize.generate_tokens(StringIO.StringIO(a).readline),
+            )
             if keys[0][0] == tokenize.NAME:
-                if b == '""': b = template.Variable('""')  # workaround bug #5270
-                else: b = parser.compile_filter(b)
+                if b == '""':
+                    b = template.Variable('""')  # workaround bug #5270
+                else:
+                    b = parser.compile_filter(b)
                 qschanges[str(a)] = b
-            else: raise ValueError
+            else:
+                raise ValueError
         except ValueError:
             raise template.TemplateSyntaxError, "Argument syntax wrong: should be key=value"
     return SetUrlParamNode(qschanges)
+
 
 register.tag('set_url_param', do_seturlparam)
